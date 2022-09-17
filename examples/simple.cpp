580940926b60fad6b980daf4299a1d69b7475c05
ury <4493070+urysegal@users.noreply.github.com>
2022-09-17 09:41:41 -0700

    More initial work

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,8 +1,24 @@
 #include "libslater.h"
 
+using namespace slater;
 
 int
 main()
 {
+    STO_Basis_Function_Info oxygen_s(43.5, 0.252,0);
+    STO_Basis_Function_Info oxygen_p(0.5, 1.29872,1);
+
+    STO_Basis_Function_Info hydrogen_s(3.15, 0.952,0);
+
+    STO_Basis_Function oxygen_1_s(oxygen_s, {0,0,-0.14142136 });
+    STO_Basis_Function oxygen_2_p(oxygen_p, {0,0,-0.14142136 });
+    STO_Basis_Function hydrogen_1_s(hydrogen_s, {0.70710678,0,0.56568542});
+    STO_Basis_Function hydrogen_2_s(hydrogen_s, {-0.70710678,0,0.56568542});
+
+    STO_Integrator engine("default");
+    engine.init();
+    integral_value result = engine.overlap(oxygen_1_s, oxygen_2_p, hydrogen_1_s, hydrogen_2_s) ;
+    engine.cleanup();
+
 	return 0;
 }
--- libslater.h
+++ libslater.h
@@ -1,10 +1,62 @@
 #pragma once
+#include <vector>
+#include <array>
 
 namespace slater {
+
 /// Initialize the library
 void libslater_global_init();
 
 /// Clean up the library
 void libslater_global_cleanup();
+
+typedef double sto_exponent_t;
+typedef double sto_coefficient_t;
+typedef unsigned int moment_quantum_number_t;
+
+typedef double spatial_coordinate_t;
+typedef std::array<spatial_coordinate_t, 3> center_t;
+
+typedef double integral_value;
+
+
+/// One STO style basis function details. STO have radial part and angular part.
+/// the radial part is the form f(r)=N*r^(n-1)*exp(-ar). N and a are the first two parameters of the constructor.
+/// the angular part is a spherical harmonic which is given by the third parameter.
+/// n is limited to be equal to l in this library, as in GTO-based basis sets.
+
+class STO_Basis_Function_Info {
+
+    sto_coefficient_t coefficient; /// Coefficient of the radial part.
+    sto_exponent_t exponent; /// exponent of the radial part.
+    moment_quantum_number_t moment_quantum_number; /// Angular moment number, also known as 'l'
+
+public:
+    /// Contruct an STO style basis function detail object.
+    /// \param coefficient_  Coefficient of the radial part.
+    /// \param exponent_ exponent of the radial part.
+    /// \param moment_quantum_number_ Angular moment number
+    STO_Basis_Function_Info(sto_coefficient_t coefficient_, sto_exponent_t exponent_, moment_quantum_number_t moment_quantum_number_);
+
+};
+
+
+/// One STO basis function, located at a given center.
+class STO_Basis_Function  {
+
+    STO_Basis_Function_Info function_info;
+    center_t center;
+
+public:
+
+    /// Construct a basis function located at a specific coordinates
+    /// \param function_info Basis function information
+    /// \param location Cartesian center of the function
+    STO_Basis_Function(STO_Basis_Function_Info function_info_, center_t location_);
+
+};
+
+
+
 }
 
