c59c385590fa87cf04019b6f733130a07329e3fe
Ury Segal <4493070+urysegal@users.noreply.github.com>
2022-10-04 14:49:13 -0700

    Skeleton for inner summation

--- include/nested_summation.h
+++ include/nested_summation.h
@@ -2,34 +2,55 @@
 
 namespace slater {
 
+
+
 template<class T, class Next_Summation, int step = 1 , typename indexing_t = int>
 class Nested_Summation
 {
-private:
-    Next_Summation inner_summation;
+protected:
+
     indexing_t from;
     indexing_t to;
+    indexing_t current_value = 0;
 
-    virtual T scaling_factor() { return dynamic_cast<T>(1); }
-    virtual T inner_sum(indexing_t i) = 0;
+    virtual T scaling_factor(indexing_t) { return 1; }
+    virtual indexing_t  get_next_sum_from() { return 0 ;}
+    virtual indexing_t  get_next_sum_to() { return -1; }
+    virtual indexing_t  get_next_sum_step() { return 1; }
+
+    virtual void rename_current_value() {}
 
 public:
 
     Nested_Summation(indexing_t from_, indexing_t to_) : from(from_), to(to_)
     {}
 
-    T get_value()
+    T get_value(indexing_t i)
     {
-        T total_sum = dynamic_cast<T>(0);
+        T total_sum = 0;
 
-        for ( indexing_t i = from ; i <= to ; i+= step) {
-            total_sum += inner_summation->get_value(i);
+        current_value = i;
+        rename_current_value();
+
+        indexing_t next_from = get_next_sum_from();
+        indexing_t next_to = get_next_sum_to();
+        indexing_t next_sum_step = get_next_sum_step();
+
+        Next_Summation inner_summation(next_from, next_to);
+        for ( indexing_t j = next_from ; j <= next_to ; j+= next_sum_step) {
+            auto scaling = scaling_factor(j);
+            total_sum +=  scaling * inner_summation->get_value(j) ;
         }
-
-        return scaling_factor() * total_sum;
+        return total_sum;
     }
 
 };
+
+class Last_Nested_Summation {
+public:
+
+};
+
 
 }
 
--- src/nested_summation.cpp
+++ src/nested_summation.cpp
@@ -2,5 +2,21 @@
 
 namespace slater {
 
+    class Integer_Sum : public Nested_Summation<long, Last_Nested_Summation> {
+
+        virtual long scaling_factor() { return current_value; }
+    public:
+        Integer_Sum(int from_, int to_) : Nested_Summation(from_, to_)
+        {}
+
+    };
+
+
+    void
+    make_sum(long i)
+    {
+        Integer_Sum ins(0,i);
+    }
+
 
 }
