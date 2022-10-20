8f27ce735ecf13bba1acf3e27e78fb8c2fa4cbc1
Gautam Luhana <gkluhana@gmail.com>
2022-10-20 12:30:28 -0700

    added tests for spherical harmonics and bfunction

--- test/test-math.cpp
+++ test/test-math.cpp
@@ -111,23 +111,58 @@ TEST_CASE( "Evaluate Spherical Harmonics ", "[b_func_engine]" ) {
     auto pi = bm::constants::pi<double>();
     Quantum_Numbers q1 = {4,2,1};
     Quantum_Numbers q2 = {4,2,-1};
+    center_t r = {1,2,1};
     double theta = pi/4;
     double phi = 0;
 
     auto Y1 = eval_spherical_harmonics(q1,theta,phi);
     auto Y2 = eval_spherical_harmonics(q2,theta,phi);
-    CHECK(abs(Y1 - std::complex<double>(-0.3862742020231,0)) < 0.001) ;
-    CHECK(abs(Y2 - std::complex<double>(0.3862742020231,0)) < 0.001) ;
+
+    CHECK(abs(Y1 - std::complex<double>(-0.38627420,0)) < 10E-9) ;
+    CHECK(abs(Y2 - std::complex<double>(0.38627420,0)) < 10E-9) ;
+    {
+        Spherical_Coordinates sc(r);
+        auto Y3 = eval_spherical_harmonics(q1,sc.theta,sc.phi);
+        CHECK(abs(Y3 - std::complex<double>(-0.128758, -0.257516)) < 10E-6);
+    }
 
 }
 
 TEST_CASE( "Evaluate B Functions ", "[b_func_engine]" ) {
     ///TEST MAY NEED FIXING
     Quantum_Numbers q1 = {4,2,1};
-    double alpha = 1;
+    double alpha = 1.0;
     center_t r{1,2,1} ;
+    {
+        //check internal boost bessel function
+        Spherical_Coordinates sc(r);
+        CHECK(bm::cyl_bessel_k(q1.n - 1.0 / 2.0, alpha * sc.radius) - 0.48190552 < 10E-9);
+    }
     B_function_Engine b_func_engine;
     auto B = b_func_engine.calculate(q1,alpha,r);
-    CHECK(abs(B - std::complex<double>(0.000324863191,0.0001452832357)) < 0.001) ;
+    CHECK(abs(B - std::complex<double>(-0.000148279, -0.000296558)) < 10E-9) ;
+
+    {   //case where result is 0
+        double alpha = 1;
+        center_t r{1,1,0} ;
+        B_function_Engine b_func_engine;
+        auto B = b_func_engine.calculate(q1,alpha,r);
+        CHECK(abs(B - std::complex<double>(0,0)) < 10E-9) ;
+    }
+    {   //case where r is 0
+        double alpha = 1;
+        center_t r{0,0,0} ;
+        B_function_Engine b_func_engine;
+        auto B = b_func_engine.calculate(q1,alpha,r);
+        CHECK(abs(B - std::complex<double>(0,0)) < 10E-9) ;
+    }/*
+    {   //case where r is 0 , n=1
+        Quantum_Numbers q2 = {1,2,1};
+        double alpha = 1;
+        center_t r{0,0,0};
+        B_function_Engine b_func_engine;
+        auto B = b_func_engine.calculate(q2,alpha,r);
+        CHECK(abs(B - std::complex<double>(1,0)) < 10E-9) ;
+    }*/
 
 }
