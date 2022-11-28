a843841b8844ad915f35a02429f9b21d75a9c064
ury <4493070+urysegal@users.noreply.github.com>
2022-11-27 18:13:48 -0800

    Use the Safouhi (complex) definition of Gaunt Co-efficient by default

--- src/gaunt.cpp
+++ src/gaunt.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "gaunt.h"
 #include "../precalculate/gaunt-table.h"
 #include <assert.h>
@@ -29,7 +30,10 @@ int Gaunt_Coefficient_Engine::get_maximal_gaunt_l()
 double Gaunt_Coefficient_Engine::calculate(const std::array<const int, 6> &args) const
 {
     auto l1 = args[0];
-    auto m1 = args[1] + L_MAX;
+
+    auto orig_m1 = args[1];
+
+    auto m1 = -orig_m1 + L_MAX;
 
     auto l2 = args[2];
     auto m2 = args[3] + L_MAX;
@@ -52,7 +56,11 @@ double Gaunt_Coefficient_Engine::calculate(const std::array<const int, 6> &args)
     assert(m3<=M_COUNT);
 
 
-    return gaunt_table[l1][l2][l3][m1][m2][m3];
+    double gaunt_coeff = gaunt_table[l1][l2][l3][m1][m2][m3];
+    gaunt_coeff *= pow(-1.0, double(orig_m1));
+
+    return gaunt_coeff;
+
 }
 
 }
--- src/homeier.cpp
+++ src/homeier.cpp
@@ -210,8 +210,7 @@ std::complex<double> Homeier_Integrator::get_gaunt_sum(const B_function_details 
     std::complex<double> total_sum = 0;
     for ( auto l = l_min ; l <= l_max ; l+=2 )
     {
-        auto gaunt_coeff = get_gaunt_coeff({q2.l, -q2.m, q1.l,q1.m, l, q2.m - q1.m});
-        gaunt_coeff *= pow(-1.0, double(q2.m));
+        auto gaunt_coeff = get_gaunt_coeff({q2.l, q2.m, q1.l,q1.m, l, q2.m - q1.m});
         std::complex<double> B_function_sum = get_B_function_sum(f1, f2, alpha, l);
         total_sum += gaunt_coeff * B_function_sum;
     }
