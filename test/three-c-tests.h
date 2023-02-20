ee1b6e4efe057303b19a776e1be82d697a18f0f1
ury <4493070+urysegal@users.noreply.github.com>
2023-02-19 19:40:27 -0800

    Add structure of 4-c tests data

--- test/three-c-tests.h
+++ test/three-c-tests.h
@@ -25,3 +25,45 @@ struct three_c_tests_t {
 		double imag;
 	} result;
 };
+
+struct four_c_tests_t {
+    int n1;
+    int l1;
+    int m1;
+    double zeta1;
+
+    int n2;
+    int l2;
+    int m2;
+    double zeta2;
+
+    int n3;
+    int l3;
+    int m3;
+    double zeta3;
+
+    int n4;
+    int l4;
+    int m4;
+    double zeta4;
+
+
+    double Ax;
+    double Ay;
+    double Az;
+
+    double Bx;
+    double By;
+    double Bz;
+
+    double Cx;
+    double Cy;
+    double Cz;
+
+    double Dx;
+    double Dy;
+    double Dz;
+
+    double dbar_res;
+    double grepw_res;
+};
