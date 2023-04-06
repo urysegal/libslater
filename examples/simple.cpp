d4e903506373183f9bfec9fd6a1eefc3f434740c
Gautam Luhana <gkluhana@gmail.com>
2023-04-06 14:35:03 -0700

    kinetic normalization push

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -22,8 +22,8 @@ main()
     STO_Basis_Function_Info hydrogen_s( 1, quantum_numbers1);
 
 
-    STO_Basis_Function oxygen_1_s(oxygen_s, {2, 0, 1});
-    STO_Basis_Function oxygen_2_p(oxygen_p, {0, 0, 1});
+    STO_Basis_Function oxygen_1_s(oxygen_s, {0, 0, 0});
+    STO_Basis_Function oxygen_2_p(oxygen_p, {0, 0, 0});
 
     STO_Basis_Function hydrogen_1_s(hydrogen_s, {0.70710678, 0, 0.56568542});
     STO_Basis_Function hydrogen_2_s(hydrogen_s, {-0.70710678, 0, 0.56568542});
